aeb3522fadbc0b792cf9f1f6f965ddef5905c1b8
syed2650 <149387292+syed2650@users.noreply.github.com>
2025-05-15 22:23:19 +0100

    Update Review.tsx

--- src/pages/Review.tsx
+++ src/pages/Review.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
-import AnalysisResults from "./review/components/AnalysisResults";
-import ResumeAnalyzer from "./review/components/ResumeAnalyzer";
+import ResumeAnalyzer from "../review/ResumeAnalyzer";
+import AnalysisResults from "../review/components/AnalysisResults";
 
 const Review = () => {
   const [feedback, setFeedback] = useState(null);
